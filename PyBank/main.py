d0c0c7cfb1f2317a461dac03a810f6e7426ce456
cruxclimb <54124486+cruxclimb@users.noreply.github.com>
2019-10-12 16:37:40 -0700

    updated file. need to export to text

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,54 +1,45 @@
-# Example outcome 
- # Financial Analysis
- # ----------------------------
- # Total Months: 86
- # Total: $38382578
- # Average  Change: $-2315.12
- # Greatest Increase in Profits: Feb-2012 ($1926159)
- # Greatest Decrease in Profits: Sep-2013 ($-2196167)
-
-
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The average of the changes in "Profit/Losses" over the entire period
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in losses (date and amount) over the entire period
-
-
 import os
 import csv
 
 with open("budget_data.csv") as file:
     read_file=csv.reader(file)
-    csvreader=list(read_file)
-    csvreader_header=csvreader[0]
-    #select second row and everything after using :
-    csvreader=csvreader[1:]
-
-total=0 
-
-for row in csvreader:
-    row_count = len(csvreader)-1
-
-    total += int(row[1])
-
-    # nest an if statement to compare each row to print the greatest increase
-    inc= max(str(row[1]))
     
-    # nest an if statement to compare each row to print the greatest decrease
-    dec= min(str(row[1]))
+    next(read_file)
+    profit = []
+    date = []
+    profit_change = []
 
-    # mean of the change from month to month
-    # create list for average change and append. 
-    # first row is not a change so "pop" the first row 
-    Avg = int(total/row_count)
+# sum of profit/losses in csv file and count total months
+    for row in read_file:
+
+        profit.append(float(row[1]))
+        date.append(row[0])
+        total_months= len(date)
+        total= sum(profit)
+    for i in range(1,len(profit)):
+
+        profit_change.append(profit[i] - profit[i-1])
+        avg_profit_change = sum(profit_change)/len(profit_change)
+
+        max_profit_change = max(profit_change)
+
+        min_profit_change = min(profit_change)
+
+        max_profit_date = str(date[profit_change.index(max(profit_change))])
+        min_profit_date = str(date[profit_change.index(min(profit_change))])
     
-
-print(f'Total Months: {row_count+1}')
+print("Financial Analysis")
+print("------------------------------------------")
+print(f'Total Months:  {total_months}')
 print (f'Total: {total}')
-print (f'Average: {Avg}')
-print (f'Greatest Increase in Profits: ({inc})')
-print (f'Greatest Decrease in Profits: ({dec})')
+print ('Average Change: $ ', round(avg_profit_change, 2))
+print ('Greatest Increase in Profits: ', max_profit_date, "($", round(max_profit_change), ")")
+print ('Greatest Decrease in Profits: ', min_profit_date, "($", round(min_profit_change), ")")
 
 
 # export text file with results
+output_path = os.path.join("PyBank.txt")
+
+with open(output_path, 'w', newline='') as csvfile:
+        csvwriter= csv.writer(csvfile, delimiter=',')
+        csvwriter.writerow(avg_profit_change)
